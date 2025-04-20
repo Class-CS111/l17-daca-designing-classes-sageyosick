d7148ea92bdc9305ffa52f6b3c26c84c93f3ffab
sageyosick <sageyosick@gmail.com>
2025-04-20 03:31:59 +0000

    daca designing classes

--- Main.java
+++ Main.java
@@ -8,6 +8,19 @@ class Main
 {
   public static void main(String[] args) 
 	{
+    DACArecipient person1, person2;
     
+    person1 = new DACArecipient();
+    person2 = new DACArecipient();
+
+    person1.setAll("Mendez", "Javier", "56-3-445", "El Salvador", 2451564, 3956753, 3956840, 'M');
+    person2.setAll("Mendez", "Javier", "56-3-445", "El Salvador", 2451564, 3956753, 3956840, 'M');
+
+    System.out.println(person1);
+    System.out.println(person2);
+    System.out.println(person1.equals(person2));
+
+    System.out.println(person1.printCard());
+
   }
 }
